06e0a3e2922071997297b12a2c40dcafa41ed0cf
Le36 <48809753+Le36@users.noreply.github.com>
2023-08-09 10:04:07 +0300

    styling for error notification

--- frontend/src/components/notification/ErrorNotification.js
+++ frontend/src/components/notification/ErrorNotification.js
@@ -1,5 +1,7 @@
 import React, {useContext} from 'react'
 import {ErrorContext} from '../../contexts/ErrorContext'
+import styles from './ErrorNotification.module.css'
+import {FaTimes} from 'react-icons/fa'
 
 const ErrorNotification = () => {
     const {errorMessage, setErrorMessage} = useContext(ErrorContext)
@@ -7,9 +9,21 @@ const ErrorNotification = () => {
     if (!errorMessage) return null
 
     return (
-        <div className="error-notification">
-            {errorMessage}
-            <button onClick={() => setErrorMessage(null)}>Dismiss</button>
+        <div className={styles.errorBackdrop}>
+            <div className={styles.errorModal}>
+                <div className={styles.titleBar}>
+                    <span>Error</span>
+                    <button className={styles.closeButton} onClick={() => setErrorMessage(null)}>
+                        <FaTimes />
+                    </button>
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.errorMessage}>{errorMessage}</div>
+                    <button className={styles.dismissButton} onClick={() => setErrorMessage(null)}>
+                        Dismiss
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }

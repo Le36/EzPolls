c7592d9505593fd57fc5087b4516945196200454
Le36 <48809753+Le36@users.noreply.github.com>
2023-08-11 16:59:38 +0300

    hero graphics and adjustments

--- frontend/src/components/home/FeatureCard.js
+++ frontend/src/components/home/FeatureCard.js
@@ -2,10 +2,12 @@ import styles from './FeatureCard.module.css'
 
 const FeatureCard = ({imgSrc, title, description}) => {
     return (
-        <div className={styles.featureCard}>
-            <img src={imgSrc} alt={title} />
-            <h3>{title}</h3>
-            <p>{description}</p>
+        <div className={styles.fadeInWrapper}>
+            <div className={styles.featureCard}>
+                <img src={imgSrc} alt={title} />
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
         </div>
     )
 }
--- frontend/src/components/home/HeroSection.js
+++ frontend/src/components/home/HeroSection.js
@@ -1,8 +1,14 @@
 import styles from './HeroSection.module.css'
+import heroImage from '../../assets/hero.jpg'
 
 const HeroSection = () => {
     return (
-        <section className={styles.hero}>
+        <section
+            className={styles.hero}
+            style={{
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4)), url(${heroImage})`,
+            }}
+        >
             <h2>Welcome to EzPolls</h2>
             <p>Create your poll in seconds</p>
         </section>

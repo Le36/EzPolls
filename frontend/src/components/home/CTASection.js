0a9726357a1be32a94cf6161eae6cd10b812df16
Le36 <48809753+Le36@users.noreply.github.com>
2023-08-11 12:26:58 +0300

    add register button to CTA

--- frontend/src/components/home/CTASection.js
+++ frontend/src/components/home/CTASection.js
@@ -4,9 +4,15 @@ const CTASection = () => {
     return (
         <section className={styles.cta}>
             <h2>Ready to dive in?</h2>
-            <a href="/create" className={styles.ctaButton}>
-                Create a Poll
-            </a>
+            <div className={styles.buttonContainer}>
+                <a href="/create" className={styles.ctaButton}>
+                    Create a Poll
+                </a>
+                or
+                <a href="/register" className={styles.ctaButton}>
+                    Register
+                </a>
+            </div>
         </section>
     )
 }
